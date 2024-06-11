e3703db00f8092b03207dbacb44fb1c106ec2ea8
gowtham <gowtham05092000@gmail.com>
2024-06-11 21:31:47 +0530

    first commit

---
+++ app.vue
@@ -0,0 +1,29 @@
+<!-- pages/index.vue -->
+<template>
+  <div>
+    <button @click="notify">Notify Me!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    notify() {
+      if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+      } else if (Notification.permission === "granted") {
+        // If it's okay let's create a notification
+        new Notification("Hi there!");
+      } else if (Notification.permission !== "denied") {
+        // Otherwise, we need to ask the user for permission
+        Notification.requestPermission().then(permission => {
+          // If the user accepts, let's create a notification
+          if (permission === "granted") {
+            new Notification("Hi there!");
+          }
+        });
+      }
+    }
+  }
+};
+</script>
